314467a91251d7bb21282c64a92adc45157dd8ee
ShingoTanikawa <shingo.tanikawa@o-n.co.jp>
2022-05-14 16:54:34 +0900

    comment

--- server.ts
+++ server.ts
@@ -6,17 +6,16 @@ import { PrismaClient } from '@prisma/client'
 const server = fastify()
 const prisma = new PrismaClient({ log: ['query', 'info', 'warn', 'error'] })
 
-//
+// http://localhost:8080/ping
 server.get('/ping', async (request, reply) => {
   return 'pong\n'
 })
 
-/*
-  SELECT `main`.`User`.`id`, `main`.`User`.`email`, `main`.`User`.`passwd` FROM `main`.`User` WHERE 1=1 LIMIT ? OFFSET ?
-  SELECT `main`.`Post`.`id`, `main`.`Post`.`createdAt`, `main`.`Post`.`updatedAt`, `main`.`Post`.`title`, `main`.`Post`.`content`, `main`.`Post`.`published`, `main`.`Post`.`authorId` FROM `main`.`Post` WHERE `main`.`Post`.`authorId` IN (?) LIMIT ? OFFSET ?
-  SELECT `main`.`Profile`.`id`, `main`.`Profile`.`name`, `main`.`Profile`.`bio`, `main`.`Profile`.`userId` FROM `main`.`Profile` WHERE `main`.`Profile`.`userId` IN (?) LIMIT ? OFFSET ?
-*/
+// http://localhost:8080/allUsers
 server.get('/allUsers', async (request, reply) => {
+  // SELECT `main`.`User`.`id`, `main`.`User`.`email`, `main`.`User`.`passwd` FROM `main`.`User` WHERE 1=1 LIMIT ? OFFSET ?
+  // SELECT `main`.`Post`.`id`, `main`.`Post`.`createdAt`, `main`.`Post`.`updatedAt`, `main`.`Post`.`title`, `main`.`Post`.`content`, `main`.`Post`.`published`, `main`.`Post`.`authorId` FROM `main`.`Post` WHERE `main`.`Post`.`authorId` IN (?) LIMIT ? OFFSET ?
+  // SELECT `main`.`Profile`.`id`, `main`.`Profile`.`name`, `main`.`Profile`.`bio`, `main`.`Profile`.`userId` FROM `main`.`Profile` WHERE `main`.`Profile`.`userId` IN (?) LIMIT ? OFFSET ?
   return await prisma.user.findMany({
     include: {
       posts: true,
@@ -25,15 +24,14 @@ server.get('/allUsers', async (request, reply) => {
   })
 })
 
-/*
-  BEGIN
-  INSERT INTO `main`.`User` (`email`) VALUES (?) RETURNING id
-  INSERT INTO `main`.`Post` (`createdAt`, `updatedAt`, `title`, `published`, `authorId`) VALUES (?,?,?,?,?) RETURNING id
-  INSERT INTO `main`.`Profile` (`name`, `bio`, `userId`) VALUES (?,?,?) RETURNING id
-  SELECT `main`.`User`.`id`, `main`.`User`.`email`, `main`.`User`.`passwd` FROM `main`.`User` WHERE `main`.`User`.`id` = ? LIMIT ? OFFSET ?
-  COMMIT
-*/
+// http://localhost:8080/createUser
 server.get('/createUser', async (request, reply) => {
+  // BEGIN
+  // INSERT INTO `main`.`User` (`email`) VALUES (?) RETURNING id
+  // INSERT INTO `main`.`Post` (`createdAt`, `updatedAt`, `title`, `published`, `authorId`) VALUES (?,?,?,?,?) RETURNING id
+  // INSERT INTO `main`.`Profile` (`name`, `bio`, `userId`) VALUES (?,?,?) RETURNING id
+  // SELECT `main`.`User`.`id`, `main`.`User`.`email`, `main`.`User`.`passwd` FROM `main`.`User` WHERE `main`.`User`.`id` = ? LIMIT ? OFFSET ?
+  // COMMIT
   const user = {
     data: {
       email: 'alice@prisma.io',
@@ -48,16 +46,15 @@ server.get('/createUser', async (request, reply) => {
   return await prisma.user.create(user)
 })
 
-/*
-  BEGIN
-  SELECT `main`.`User`.`id` FROM `main`.`User` WHERE `main`.`User`.`id` = ?
-  UPDATE `main`.`User` SET `passwd` = ? WHERE `main`.`User`.`id` IN (?)
-  SELECT `main`.`Post`.`id`, `main`.`Post`.`authorId` FROM `main`.`Post` WHERE (`main`.`Post`.`id` = ? AND `main`.`Post`.`authorId` IN (?)) LIMIT ? OFFSET ?
-  UPDATE `main`.`Post` SET `published` = ?, `updatedAt` = ? WHERE `main`.`Post`.`id` IN (?)
-  SELECT `main`.`User`.`id`, `main`.`User`.`email`, `main`.`User`.`passwd` FROM `main`.`User` WHERE `main`.`User`.`id` = ? LIMIT ? OFFSET ?
-  COMMIT
-*/
+// http://localhost:8080/updateUser
 server.get('/updateUser', async (request, reply) => {
+  // BEGIN
+  // SELECT `main`.`User`.`id` FROM `main`.`User` WHERE `main`.`User`.`id` = ?
+  // UPDATE `main`.`User` SET `passwd` = ? WHERE `main`.`User`.`id` IN (?)
+  // SELECT `main`.`Post`.`id`, `main`.`Post`.`authorId` FROM `main`.`Post` WHERE (`main`.`Post`.`id` = ? AND `main`.`Post`.`authorId` IN (?)) LIMIT ? OFFSET ?
+  // UPDATE `main`.`Post` SET `published` = ?, `updatedAt` = ? WHERE `main`.`Post`.`id` IN (?)
+  // SELECT `main`.`User`.`id`, `main`.`User`.`email`, `main`.`User`.`passwd` FROM `main`.`User` WHERE `main`.`User`.`id` = ? LIMIT ? OFFSET ?
+  // COMMIT
   const passwd = await hash('pa55w0rd', 10)
   return await prisma.user.update({
     where: { id: 1 },
@@ -73,10 +70,9 @@ server.get('/updateUser', async (request, reply) => {
   })
 })
 
-/*
-  SELECT `main`.`User`.`id`, `main`.`User`.`email`, `main`.`User`.`passwd` FROM `main`.`User` WHERE `main`.`User`.`id` = ? LIMIT ? OFFSET ?
-*/
+// http://localhost:8080/comparePasswd
 server.get('/comparePasswd', async (request, reply) => {
+  // SELECT `main`.`User`.`id`, `main`.`User`.`email`, `main`.`User`.`passwd` FROM `main`.`User` WHERE `main`.`User`.`id` = ? LIMIT ? OFFSET ?
   const user = await prisma.user.findUnique({
     where: { id: 1 },
   })
